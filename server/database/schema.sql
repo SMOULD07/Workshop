d9acf75e4cb29083a6f5d65278574385d0f8c464
Salma-D38 <s.desmazieres@yahoo.fr>
2024-09-24 15:37:38 +0200

    suggestion

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,48 +1,48 @@
-CREATE TABLE Utilisateur (
+CREATE TABLE utilisateur (
     ID_utilisateur INT AUTO_INCREMENT PRIMARY KEY,
     Nom VARCHAR(255) NOT NULL,
-    Rôle ENUM('étudiant', 'administrateur') NOT NULL
+    Rôle ENUM('etudiant', 'administrateur') NOT NULL
 );
 
 
-CREATE TABLE Catégorie (
-    ID_catégorie INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE categorie (
+    ID_categorie INT AUTO_INCREMENT PRIMARY KEY,
     Nom VARCHAR(255) NOT NULL,
     Description TEXT
 );
 
 
-CREATE TABLE Suggestion (
+CREATE TABLE suggestion (
     ID_suggestion INT AUTO_INCREMENT PRIMARY KEY,
     Titre VARCHAR(255) NOT NULL,
     Description TEXT,
     ID_utilisateur INT,
-    ID_catégorie INT,
-    CONSTRAINT FK_Utilisateur_Suggestion FOREIGN KEY (ID_utilisateur) REFERENCES Utilisateur(ID_utilisateur) ON DELETE CASCADE,
-    CONSTRAINT FK_Catégorie_Suggestion FOREIGN KEY (ID_catégorie) REFERENCES Catégorie(ID_catégorie) ON DELETE SET NULL
+    ID_categorie INT,
+    CONSTRAINT FK_utilisateur_Suggestion FOREIGN KEY (ID_utilisateur) REFERENCES utilisateur(ID_utilisateur) ON DELETE CASCADE,
+    CONSTRAINT FK_categorie_Suggestion FOREIGN KEY (ID_categorie) REFERENCES categorie(ID_categorie) ON DELETE SET NULL
 );
 
 
-CREATE TABLE Tag (
+CREATE TABLE tag (
     ID_tag INT AUTO_INCREMENT PRIMARY KEY,
     Nom VARCHAR(255) NOT NULL
 );
 
 
-CREATE TABLE Suggestion_Tag (
+CREATE TABLE suggestion_tag (
     ID_suggestion INT,
     ID_tag INT,
     PRIMARY KEY (ID_suggestion, ID_tag),
-    CONSTRAINT FK_Suggestion_Tag_S FOREIGN KEY (ID_suggestion) REFERENCES Suggestion(ID_suggestion) ON DELETE CASCADE,
-    CONSTRAINT FK_Suggestion_Tag_T FOREIGN KEY (ID_tag) REFERENCES Tag(ID_tag) ON DELETE CASCADE
+    CONSTRAINT FK_suggestion_tag_S FOREIGN KEY (ID_suggestion) REFERENCES suggestion(ID_suggestion) ON DELETE CASCADE,
+    CONSTRAINT FK_suggestion_tag_T FOREIGN KEY (ID_tag) REFERENCES tag(ID_tag) ON DELETE CASCADE
 );
 
 
-CREATE TABLE Vote (
+CREATE TABLE vote (
     ID_vote INT AUTO_INCREMENT PRIMARY KEY,
     ID_suggestion INT,
     ID_utilisateur INT,
     Type_de_vote ENUM('pour', 'contre') NOT NULL,
-    CONSTRAINT FK_Vote_Suggestion FOREIGN KEY (ID_suggestion) REFERENCES Suggestion(ID_suggestion) ON DELETE CASCADE,
-    CONSTRAINT FK_Vote_Utilisateur FOREIGN KEY (ID_utilisateur) REFERENCES Utilisateur(ID_utilisateur) ON DELETE CASCADE
+    CONSTRAINT FK_vote_suggestion FOREIGN KEY (ID_suggestion) REFERENCES Suggestion(ID_suggestion) ON DELETE CASCADE,
+    CONSTRAINT FK_vote_utilisateur FOREIGN KEY (ID_utilisateur) REFERENCES Utilisateur(ID_utilisateur) ON DELETE CASCADE
 );
